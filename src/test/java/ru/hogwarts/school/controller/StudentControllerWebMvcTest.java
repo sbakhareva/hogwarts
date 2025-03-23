45d80e11717bc74b200ebd499e06061d84523d07
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-03-23 22:12:13 +0300

    дз по тестам Mvc

--- src/test/java/ru/hogwarts/school/controller/StudentControllerWebMvcTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerWebMvcTest.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -7,7 +8,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.hogwarts.school.dto.StudentDTOMapper;
 import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.StudentRepository;
-import ru.hogwarts.school.service.FacultyService;
 import ru.hogwarts.school.service.StudentService;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,4 +24,59 @@ class StudentControllerWebMvcTest {
     private StudentDTOMapper studentDTOMapper;
     @MockitoBean
     private StudentService studentService;
+
+    @Test
+    void addStudentTest() {
+
+    }
+
+    @Test
+    void getStudentByIdTest() {
+
+    }
+
+    @Test
+    void getAllStudentsTest() {
+
+    }
+
+    @Test
+    void removeStudentTest() {
+
+    }
+
+    @Test
+    void editStudentTest() {
+
+    }
+
+    @Test
+    void sortStudentsByAgeTest() {
+
+    }
+
+    @Test
+    void findBetweenAgeTest() {
+
+    }
+
+    @Test
+    void getFacultyTest() {
+
+    }
+
+    @Test
+    void uploadAvatarTest() {
+        //по ситуации
+    }
+
+    @Test
+    void downloadAvatarTest() {
+        //по ситуации
+    }
+
+    @Test
+    void downloadPreviewTest() {
+        //по ситуации
+    }
 }
