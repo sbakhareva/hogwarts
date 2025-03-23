013512d08375ac21243e581aa8309aa3ca280444
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-03-23 20:35:01 +0300

    дз по тестам TestRestTemplate с H2

--- src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.*;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.util.LinkedMultiValueMap;
@@ -20,12 +21,16 @@ import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.FacultyRepository;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpMethod.GET;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -191,63 +196,100 @@ class StudentControllerTest {
                 .contains("Гриффиндор");
     }
 
-//    @Test
-//    void uploadAvatar() throws IOException {
-//        Faculty f = facultyRepository.findByNameIgnoreCaseContains("Гриффиндор").get();
-//
-//        Student s = new Student();
-//        s.setName("Barbie");
-//        s.setAge(17);
-//        s.setFaculty(f);
-//        studentController.addStudent(s);
-//
-//        byte[] bytes = Files.readAllBytes(Path.of("C:/Users/Снежана/IdeaProjects/school/src/test/resources/test.jpg"));
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-//
-//        Avatar studentAvatar = new Avatar();
-//        studentAvatar.setStudent(s);
-//        studentAvatar.setFilePath("/1.jpg");
-//        studentAvatar.setFileSize(studentAvatar.getFileSize());
-//        studentAvatar.setMediaType("image/jpg");
-//        studentAvatar.setPreview(bytes);
-//
-//        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-//        body.add("file", new FileSystemResource("C:/Users/Снежана/IdeaProjects/school/src/test/resources/test.jpg"));
-//        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-//
-//        ResponseEntity response = testRestTemplate.exchange(
-//                "http://localhost:" + port + "/school/student/" + s.getId() + "/avatar",
-//                HttpMethod.POST,
-//                requestEntity,
-//                String.class
-//        );
-//        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//    }
+    @Test
+    void uploadAvatar() {
+        Faculty f = new Faculty("Гриффиндор", "красный");
+        facultyRepository.save(f);
 
-//    @Test
-//    void downloadPreview() throws IOException {
-//        Faculty f = facultyRepository.findByNameIgnoreCaseContains("Гриффиндор").get();
-//
-//        Student s = new Student();
-//        s.setName("Barbie");
-//        s.setAge(17);
-//        s.setFaculty(f);
-//        studentController.addStudent(s);
-//
-//        byte[] bytes = Files.readAllBytes(Path.of("C:/Users/Снежана/IdeaProjects/school/src/test/resources/test.jpg"));
-//
-//        Avatar studentAvatar = new Avatar();
-//        studentAvatar.setStudent(s);
-//        studentAvatar.setFilePath("/1.jpg");
-//        studentAvatar.setFileSize(studentAvatar.getFileSize());
-//        studentAvatar.setMediaType("image/jpg");
-//        studentAvatar.setPreview(bytes);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.parseMediaType(studentAvatar.getMediaType()));
-//        headers.setContentLength(studentAvatar.getPreview().length);
-//    }
-    //
-}
+        Student s = new Student();
+        s.setName("Barbie");
+        s.setAge(17);
+        s.setFaculty(f);
+        studentController.addStudent(s);
+
+        File avatarFile = new File("C:/Users/Снежана/IdeaProjects/school/src/test/resources/test.jpg");
+        FileSystemResource fileResource = new FileSystemResource(avatarFile);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("avatar", fileResource);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = testRestTemplate.exchange(
+                "/school/student/" + s.getId() + "/avatar",
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertTrue(response.getBody().contains("добавлен аватар"));
+    }
+
+    @Test
+    void downloadPreview() throws IOException {
+        Faculty f = new Faculty("Гриффиндор", "красный");
+        facultyRepository.save(f);
+
+        Student s = new Student();
+        s.setName("Barbie");
+        s.setAge(17);
+        s.setFaculty(f);
+        studentController.addStudent(s);
+
+        byte[] bytes = Files.readAllBytes(Path.of("C:/Users/Снежана/IdeaProjects/school/src/test/resources/test.jpg"));
+
+        Avatar studentAvatar = new Avatar();
+        studentAvatar.setStudent(s);
+        studentAvatar.setFilePath("/1.jpg");
+        studentAvatar.setFileSize(studentAvatar.getFileSize());
+        studentAvatar.setMediaType("image/jpg");
+        studentAvatar.setPreview(bytes);
+
+        avatarRepository.save(studentAvatar);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(studentAvatar.getMediaType()));
+        headers.setContentLength(studentAvatar.getPreview().length);
+
+        ResponseEntity<byte[]> response = testRestTemplate.getForEntity(
+                "http://localhost:" + port + "/school/student/" + s.getId() + "/avatar/preview",
+                byte[].class
+        );
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void downloadAvatarTest() throws IOException {
+        Faculty f = new Faculty("Гриффиндор", "красный");
+        facultyRepository.save(f);
+
+        Student s = new Student();
+        s.setName("Barbie");
+        s.setAge(17);
+        s.setFaculty(f);
+        studentController.addStudent(s);
+
+        File avatarFile = new File("C:/Users/Снежана/IdeaProjects/school/src/test/resources/test.jpg");
+        byte[] bytes = Files.readAllBytes(Path.of("C:/Users/Снежана/IdeaProjects/school/src/test/resources/test.jpg"));
+
+        Avatar studentAvatar = new Avatar();
+        studentAvatar.setStudent(s);
+        studentAvatar.setFilePath(avatarFile.getAbsolutePath());
+        studentAvatar.setFileSize(avatarFile.length());
+        studentAvatar.setMediaType("image/jpg");
+        studentAvatar.setPreview(bytes);
+
+        avatarRepository.save(studentAvatar);
+
+        ResponseEntity<byte[]> response = testRestTemplate.getForEntity(
+                "/school/student/" + s.getId() + "/getAvatar",
+                byte[].class
+        );
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertNotNull(response.getBody());
+        assertEquals("image/jpg", response.getHeaders().getContentType().toString());
+        assertEquals(bytes.length, response.getHeaders().getContentLength());
+        }
+    }
