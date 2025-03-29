8ad1f57572744550fc9e5d3a3511c351d23bdeb8
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-03-29 21:20:37 +0300

    интеграционные тесты

--- src/main/java/ru/hogwarts/school/model/exception/HogwartsControllerAdvice.java
+++ src/main/java/ru/hogwarts/school/model/exception/HogwartsControllerAdvice.java
@@ -1,8 +1,11 @@
 package ru.hogwarts.school.model.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class HogwartsControllerAdvice {
