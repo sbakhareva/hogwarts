b64bab9a4a516055433dd04a15bf2ca1961cc314
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-05-03 18:28:56 +0300

    рабочий и обновленный

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -1,7 +1,6 @@
 package ru.hogwarts.school.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import java.util.List;
 import java.util.Objects;
